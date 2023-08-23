376ee025d52c8fa9b3a2c530f857557db17f3481
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-08-23 16:29:30 +0300

    Add Base "ViewModel" and "View"

---
+++ SandboxForSolvingProgrammingProblems/ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxForSolvingProgrammingProblems.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel selectedViewModel;
+
+        public MainViewModel()
+        {
+            this.selectedViewModel = new WorkspaceViewModel();
+        }
+
+        public BaseViewModel SelectedViewModel
+        {
+            get
+            {
+                return selectedViewModel;
+            }
+            set
+            {
+                if (value is not BaseViewModel)
+                {
+                    selectedViewModel = new WorkspaceViewModel();
+                }
+                selectedViewModel = value;
+            }
+        }
+    }
+}
