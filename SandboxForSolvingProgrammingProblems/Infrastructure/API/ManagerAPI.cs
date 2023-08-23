baa68a108cb3ab3b73afcc6a1873398e13c38db6
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-08-24 02:40:09 +0300

    Upload APIKey with json.

---
+++ SandboxForSolvingProgrammingProblems/Infrastructure/API/ManagerAPI.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxForSolvingProgrammingProblems.Infrastructure.API
+{
+    abstract class ManagerAPI
+    {
+        protected HttpClient httpClient;
+
+        public ManagerAPI()
+        {
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            this.httpClient = new HttpClient();
+            this.httpClient.DefaultRequestHeaders.Accept.Clear();
+            this.httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident / 6.0)");
+        }
+    }
+}
---
+++ SandboxForSolvingProgrammingProblems/Infrastructure/API/ManagerSandboxAPI.cs
@@ -0,0 +1,50 @@
+﻿using SandboxForSolvingProgrammingProblems.Infrastructure.Secret;
+using SandboxForSolvingProgrammingProblems.Infrastructure.Secret.Interface;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandboxForSolvingProgrammingProblems.Infrastructure.API
+{
+    class ManagerSandboxAPI : ManagerAPI
+    {
+        IGetAPIKey getAPIKey;
+
+        private readonly string apikey;
+
+        private static ManagerSandboxAPI instance;
+        private static ManagerSandboxAPI Instance
+        {
+            get
+            {
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+
+        public ManagerSandboxAPI(): base()
+        {
+            getAPIKey = new SecretKey();
+            apikey = getAPIKey.GetAPIKey();
+
+            this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/Json"));
+            this.httpClient.DefaultRequestHeaders.Add("client-secret", apikey);
+        }
+
+        public static ManagerSandboxAPI GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new ManagerSandboxAPI();
+            }
+            return Instance;
+        }
+    }
+}
