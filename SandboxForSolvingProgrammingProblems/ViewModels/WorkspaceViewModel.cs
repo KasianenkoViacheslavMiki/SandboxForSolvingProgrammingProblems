c42f111c151cf8c1588738fd7b11ff877749427e
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-08-23 17:38:48 +0300

    Add Base "SideViewModel" and Blank interface

--- SandboxForSolvingProgrammingProblems/ViewModels/WorkspaceViewModel.cs
+++ SandboxForSolvingProgrammingProblems/ViewModels/WorkspaceViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SandboxForSolvingProgrammingProblems.ViewModels.SideMenu;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,27 @@ namespace SandboxForSolvingProgrammingProblems.ViewModels
 {
     class WorkspaceViewModel : BaseViewModel
     {
-        
+        private BaseSideViewModel selectedSideView;
+
+        public WorkspaceViewModel()
+        {
+            this.selectedSideView = new ManualSettingsSideViewModel();
+        }
+
+        public BaseSideViewModel SelectedSideView
+        {
+            get
+            {
+                return selectedSideView;
+            }
+            set
+            {
+                if (value is not BaseSideViewModel)
+                {
+                    selectedSideView = new ManualSettingsSideViewModel();
+                }
+                selectedSideView = value;
+            }
+        }
     }
 }
