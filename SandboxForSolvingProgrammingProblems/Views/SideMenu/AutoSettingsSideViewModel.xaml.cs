185ac2ca880b35bacff2e8bd58d5f6d4812f2e37
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-08-27 14:52:29 +0300

    Add new ViewModel and View

---
+++ SandboxForSolvingProgrammingProblems/Views/SideMenu/AutoSettingsSideViewModel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SandboxForSolvingProgrammingProblems.Views.SideMenu
+{
+    /// <summary>
+    /// Interaction logic for AutoSettingsSideViewModel.xaml
+    /// </summary>
+    public partial class AutoSettingsSideViewModel : UserControl
+    {
+        public AutoSettingsSideViewModel()
+        {
+            InitializeComponent();
+        }
+    }
+}
