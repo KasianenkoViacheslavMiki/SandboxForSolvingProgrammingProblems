79554402f2aee1ef1d8afdecc13518611456ecaf
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-08-27 22:45:36 +0300

    Filled SelectedTaskView

--- SandboxForSolvingProgrammingProblems/ViewModels/SideMenu/AutoSettingsViewModel/SelectedTaskViewModel.cs
+++ SandboxForSolvingProgrammingProblems/ViewModels/SideMenu/AutoSettingsViewModel/SelectedTaskViewModel.cs
@@ -1,9 +1,11 @@
-﻿using SandboxForSolvingProgrammingProblems.Models;
+﻿using SandboxForSolvingProgrammingProblems.Infrastructure;
+using SandboxForSolvingProgrammingProblems.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SandboxForSolvingProgrammingProblems.ViewModels.SideMenu.AutoSettingsModel
 {
@@ -12,12 +14,42 @@ namespace SandboxForSolvingProgrammingProblems.ViewModels.SideMenu.AutoSettingsM
         private RequestEvaluation requestEvaluation;
         private Question question;
 
+        private ICommand returnCommand;
+        
+        public ICommand ReturnCommand
+        {
+            get
+            {
+                return returnCommand ?? (returnCommand = new RelayCommand(async obj =>
+                {
+                    OnReturnToListTask("");
+                }));
+            }
+        }
 
         //Copy link to requestEvaluation in Heap
         public SelectedTaskViewModel(RequestEvaluation requestEvaluation, Question question)
         {
             this.requestEvaluation = requestEvaluation;
             this.question = question;
+            OnPropertyChanged(nameof(Title));
+            OnPropertyChanged(nameof(Content));
+        }
+
+
+        public string Title
+        {
+            get
+            {
+                return this.question.Title;
+            }
+        }
+        public string Content
+        {
+            get
+            {
+                return this.question.Content;
+            }
         }
 
         public int MemoryLimit
